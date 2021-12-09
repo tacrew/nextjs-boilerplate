e18ba9e6870a2aa1af4e52ae89d68a73bead63aa
Tacrew <t.murakami411@gmail.com>
2021-12-09 20:46:49 +0900

    setup storybook (#36)
    * chore: setup storybook with webpack 5
    
    * chore: add Button componet storybook

---
+++ .storybook/main.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    }
+    return config
+  },
+}
